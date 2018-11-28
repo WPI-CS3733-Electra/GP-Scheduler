a11eb656939bf7ac6756e8c1b1299fa410c8ae36
Ziqian Zeng <31387886+zzq980398374@users.noreply.github.com>
2018-11-28 15:38:05 -0500

    From ConstantDAO to schedule DAO
    From ConstantDAO to schedule DAO

--- src/main/java/com/amazonaws/db/SchedulerDAO.java
+++ src/main/java/com/amazonaws/db/SchedulerDAO.java
@@ -4,17 +4,47 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.amazonaws.model.Schedule;
+
 public class SchedulerDAO {
 
 	java.sql.Connection conn;
 
-    public SchedulerDAO() {
-    	try  {
-    		conn = DatabaseUtil.connect();
-    	} catch (Exception e) {
-    		conn = null;
-    	}
-    }
+	public SchedulerDAO() {
+		try {
+			conn = DatabaseUtil.connect();
+		} catch (Exception e) {
+			conn = null;
+		}
+	}
 
-  
+	public Schedule getSchedule(String suuid) throws Exception {
+
+		try {
+			Schedule schedule = new Schedule();
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM Schedule WHERE scheduleUUID=?;");
+			ps.setString(1, suuid);
+			ResultSet resultSet = ps.executeQuery();
+
+			while (resultSet.next()) {
+				schedule.setId(resultSet.getString("scheduleUUID"));
+				schedule.setName(resultSet.getString("name"));
+				schedule.setAuthor(resultSet.getString("author"));
+				schedule.setSecretCode(resultSet.getString("secretCode"));
+				schedule.setCreatedDate(resultSet.getDate("createdDate").toString());
+				schedule.setTimePeriod(resultSet.getInt("timePeriod"));
+				schedule.setStartTime(resultSet.getTime("startTime").toString());
+				schedule.setEndTime(resultSet.getTime("endTime").toString());
+			}
+			resultSet.close();
+			ps.close();
+
+			return schedule;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Failed in getting schedule: " + e.getMessage());
+		}
+	}
+
 }
