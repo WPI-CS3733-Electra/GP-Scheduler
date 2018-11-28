5ed50348cda78d85decc4c01288d38cf4fa1c128
Seeker Wong <Seeker Wong@192.168.1.2>
2018-11-28 00:59:55 -0500

    finish the cScheduleHandler

--- src/main/java/com/amazonaws/lambda/demo/CreateScheduleHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateScheduleHandler.java
@@ -1,5 +1,11 @@
 package com.amazonaws.lambda.demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,15 +18,23 @@ import org.joda.time.LocalTime;
 import org.joda.time.Minutes;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import com.amazonaws.db.SchedulerDAO;
 import com.amazonaws.model.Day;
 import com.amazonaws.model.Schedule;
 import com.amazonaws.model.Timeslot;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
 
-public class CreateScheduleHandler {
+public class CreateScheduleHandler implements RequestStreamHandler{
 	public LambdaLogger logger = null;
+	String sId = this.genUUIDString();
+	String sCode = this.genUUIDString();
+	String rCode = this.genUUIDString();
 
 	/** Load from RDS, if it exists
 	 * 
@@ -29,9 +43,6 @@ public class CreateScheduleHandler {
 	boolean createSchedule(String name, String author, String startDate, String endDate, String startTime, String endTime, int timePeriod) throws Exception {
 		if (logger != null) { logger.log("in createSchedule"); }
 		SchedulerDAO dao = new SchedulerDAO();
-		String sId = this.genUUIDString();
-		String sCode = this.genUUIDString();
-		String rCode = this.genUUIDString();
 		int numOfDays = this.daysBetweenDates(startDate, endDate);
 		int numOfMins = this.minutesBetweenTimes(startTime, endTime);
 		int numOfTs = numOfMins / timePeriod;
@@ -100,4 +111,77 @@ public class CreateScheduleHandler {
 		return LocalDate.parse(startDate).plusDays(index);
 	}
 	
+	
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		logger = context.getLogger();
+		logger.log("Loading Java Lambda handler to create constant");
+
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type",  "application/json");  // not sure if needed anymore?
+		headerJson.put("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    headerJson.put("Access-Control-Allow-Origin",  "*");
+	        
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
+
+		CreateScheduleResponse response = null;
+		
+		// extract body from incoming HTTP POST request. If any error, then return 422 error
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			JSONParser parser = new JSONParser();
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("event:" + event.toJSONString());
+			
+			String method = (String) event.get("httpMethod");
+			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
+				logger.log("Options request");
+				response = new CreateScheduleResponse("name", 200);  // OPTIONS needs a 200 response
+		        responseJson.put("body", new Gson().toJson(response));
+		        processed = true;
+		        body = null;
+			} else {
+				body = (String)event.get("body");
+				if (body == null) {
+					body = event.toJSONString();  // this is only here to make testing easier
+				}
+			}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			response = new CreateScheduleResponse("Bad Request:" + pe.getMessage(), 422);  // unable to process input
+	        responseJson.put("body", new Gson().toJson(response));
+	        processed = true;
+	        body = null;
+		}
+
+		if (!processed) {
+			CreateScheduleRequest req = new Gson().fromJson(body, CreateScheduleRequest.class);
+			logger.log(req.toString());
+
+			CreateScheduleResponse resp;
+			try {
+				if (createSchedule(req.name, req.author, req.startDate,req.endDate, req.startTime, req.endTime, req.timePeriod)) {
+					resp = new CreateScheduleResponse("Successfully defined constant:" + req.name,sCode,rCode);
+				} else {
+					resp = new CreateScheduleResponse("Unable to create Schedule: " + req.name, 422);
+				}
+			} catch (Exception e) {
+				resp = new CreateScheduleResponse("Unable to create Schedule: " + req.name + "(" + e.getMessage() + ")", 403);
+			}
+
+			// compute proper response
+	        responseJson.put("body", new Gson().toJson(resp));  
+		}
+		
+        logger.log("end result:" + responseJson.toJSONString());
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());  
+        writer.close();
+	}
+}
+
 }
